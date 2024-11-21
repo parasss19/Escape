9155d53ffa41feacd3442d4a3fb881e9a1180b7c
Paras <mehtaparas1901@gmail.com>
2024-11-21 15:48:38 +0530

    removed vite bundler and install tailwind using cli

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-   base: '/Tailwind-Project/',
-   css: {
-    postcss: './postcss.config.js', // Path to your PostCSS configuration
-  },
-});
